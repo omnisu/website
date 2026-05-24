d45446c3935ae6c855fa50fed34acc104cda39bd
Egor Morozov <egor.morozov.dev@gmail.com>
2026-05-24 13:50:14 +0300

    chore: init repo

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1 className="text-xl">Seems to be working 💡</h1>
+      <p className="opacity-50 mt-1">egormorozov.dev</p>
+    </div>
+  );
+}
